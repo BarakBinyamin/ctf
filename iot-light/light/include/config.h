061ce0ed4f9d7c11d5d9394869125519509681ae
Barak Binaymin <barak.binyamin555@gmail.com>
2023-11-29 14:20:39 -0500

    config update defualts

---
+++ iot-light/light/include/config.h
@@ -0,0 +1,5 @@
+// on mac: /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I
+#define SSID     "wifi"                            //"MySpectrumWiFib8-2G"
+#define PASS     "pass"                        //"widebunny739"
+#define WSSERVER "server"   
+#define WSPORT   80                                   
