06b96b84b75cc6e2be5be791abdbca90bbaf9032
Barak Binaymin <barak.binyamin555@gmail.com>
2023-12-14 00:01:16 -0500

    Good

--- iot-light/light/include/config.h
+++
@@ -1,4 +0,0 @@
-#define SSID     "SSID"                           
-#define PASS     "PASS"                        
-#define WSSERVER "IP_ADDRESS_OR_HOSTNAME_OF_SERVER"   
-#define WSPORT   80                               
